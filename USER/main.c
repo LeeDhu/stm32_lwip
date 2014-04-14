0f36af8f2d70a3524134e1d95fac93597eff8497
liming <Lee.dhucst@gmail.com>
2014-04-14 14:00:47 +0800

    init

---
+++ USER/main.c
@@ -0,0 +1,14 @@
+/**
+ * @file main.c
+ * @brief 
+ * @author Li Ming <Lee.dhucst@gmail.com>
+ * @url http://blog.csdn.net/lim680
+ * @version V1.0
+ * @date 2014-04-13
+ * @Copyright (c) DHU, CST.  All Rights Reserved
+ */
+
+
+int main()
+{
+}
